90da6f2280954f2e20e8a1ebb047fb6c1b6fd141
mikhaeelatefrizk <180051136+mikhaeelatefrizk@users.noreply.github.com>
2025-10-18 17:19:11 -0400

    Update import path in client/src/const.ts after moving shared directory

--- client/src/const.ts
+++ client/src/const.ts
@@ -1,4 +1,4 @@
-export { COOKIE_NAME, ONE_YEAR_MS } from "@shared/const";
+export { COOKIE_NAME, ONE_YEAR_MS } from "./shared/const";
 
 export const APP_TITLE = import.meta.env.VITE_APP_TITLE || "App";
 
